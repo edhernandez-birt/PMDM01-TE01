0f01b0d7d0e063d95eb4ab31014287df6b26f08a
Eduardo Hernandez <edhernandez@birt.eus>
2021-10-04 18:34:50 +0200

    Cambio tab3 por ayuda

--- src/app/ayuda/ayuda.page.spec.ts
+++ src/app/ayuda/ayuda.page.spec.ts
@@ -2,19 +2,19 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab3Page } from './tab3.page';
+import { ayudaPage } from './ayuda.page';
 
-describe('Tab3Page', () => {
-  let component: Tab3Page;
-  let fixture: ComponentFixture<Tab3Page>;
+describe('ayudaPage', () => {
+  let component: ayudaPage;
+  let fixture: ComponentFixture<ayudaPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab3Page],
+      declarations: [ayudaPage],
       imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab3Page);
+    fixture = TestBed.createComponent(ayudaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
