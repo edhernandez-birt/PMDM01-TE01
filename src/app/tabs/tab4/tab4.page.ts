5a6f6f56e205dc2cc86c4a4d27f9768530475865
Eduardo Hernandez <edhernandez@birt.eus>
2021-10-18 23:07:27 +0200

    Retoques tabs y primera version adivinar numero

--- src/app/tabs/tab4/tab4.page.ts
+++ src/app/tabs/tab4/tab4.page.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
+  numoculto: number = 1;
+  numformulario: number =0;
+  mensaje: string = "¿Vas a acertar rápido?"
+  intentos: number =0;
+
   constructor() { }
 
+  compruebaNumero(){
+    console.log("Botón clickado");
+    console.log(this.numformulario);
+    console.log(this.numoculto);
+    this.intentos++;
+    if (this.numoculto>this.numformulario){
+      this.mensaje="El número es mayor";
+    }else if (this.numoculto<this.numformulario){
+      this.mensaje="El número es menor";
+    } else {
+      this.mensaje="Felicidades. Acertaste en "+this.intentos+" intentos";
+    }
+  }
+
   ngOnInit() {
+    //Generaramos número random al cargar
+    this.numoculto =  Math.round(Math.random() * 100);
+    console.log(this.numoculto);
   }
 
 }
